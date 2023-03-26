fedb38637bcd288080c4b302cd6c9739a55ea9e1
benultimate1 <benjimensah@gmail.com>
2023-03-26 11:54:17 +0200

    first commit

---
+++ javaScriptCodes/index.js
@@ -0,0 +1,19 @@
+// let color=['blue','green'];
+// color[1]='yellow';
+// color[2]='black';
+// console.log(color[2]);
+// console.log(color[1]);
+// function names(fistName,lastName){
+// console.log('hello '+fistName+'  '+lastName);
+// }
+// names('ben','mensah');
+// function addd(a,b){
+//     return a*b;
+//     }
+//     console.log(addd(33,44));
+let person ={
+name: 'benj',
+age: 47,
+heigth:23.34
+};
+console.log(person.age);
---
+++ javaScriptCodes/index1.js
@@ -0,0 +1,15 @@
+let studentsInformation={
+    name:"benjamin mensah",
+    age:30,
+    programe:"MIS"
+};
+console.log(studentsInformation)
+console.log(studentsInformation.age)
+let infor=['ben','dan','mike',12];
+infor[4]='bird';
+infor[1]='love';
+console.log(infor) 
+function calculations(b){
+return (b)**0.5;
+}
+console.log(calculations(9));
